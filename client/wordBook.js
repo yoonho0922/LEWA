1430fb3723d22d9c4e613dc31c262b65e498cae3
hhz2000 <hhz2000@naver.com>
2020-05-20 00:09:33 +0900

    단어장 만드는중

--- client/wordBook.js
+++ client/wordBook.js
@@ -1,1 +1,16 @@
 FlowRouter.template('/wordBook', 'wordBook');
+
+Template.wordBook.helpers({
+    words: function () {
+
+        var words_list = new Array();
+
+        // words_list.push(DB_WORDS.findOne({user_id: Meteor.user()._id}));
+
+        var listed = DB_WORDS.findAll({user_id: Meteor.user()._id})
+        listed.forEach(function (element) {
+            words_list.push(DB_WORDS.findOne({word: element.word}));
+        });
+        return words_list;
+    },
+})
