1d55742210de0011e30ec31e5afbcc2f362a974e
hhz2000 <hhz2000@naver.com>
2020-05-19 22:10:08 +0900

    스크랩페이지

--- client/clipping.js
+++ client/clipping.js
@@ -4,23 +4,16 @@ Template.clipping.helpers({
     boards: function () {
 
         var clip_articles = new Array();
-        var cliping = DB_CLIPS.findAll({user_id: Meteor.user()._id})
-        cliping.forEach(function (element) {
-            clip_articles.push(DB_CLIPS.findOne({_id: element.post_id}));
+        var clipped = DB_CLIPS.findAll({user_id: Meteor.user()._id})
+        clipped.forEach(function (element) {
+            clip_articles.push(DB_ARTICLES.findOne({_id: element.post_id}));
         });
-        var boards = new Array();
-
-////몰ㄹ라 모르겠음
-
+        return clip_articles;
     },
-
-    //     return DB_CLIPS.findAll({}, {sort: {viewCount: -1}});
-    //     //기사들을 조회수의 내림차순으로 return
-    // },
-    link: function() {
+    image_link: function() {
         // 저장 된 이미지 링크를 반환
-        return DB_FILES.findOne({_id: this.file_id}).link()
-    }
+        return DB_FILES.findOne({_id: this.image}).link();
+    },
 });
 
 Template.clipping.events({
