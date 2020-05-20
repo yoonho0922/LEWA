723235768828405cb2faf733be4c6d177eb30458
Jungahn Yang <duckzza0921@gmail.com>
2020-05-20 11:40:54 +0900

    articleList working on pagination

--- client/articleList.js
+++ client/articleList.js
@@ -1,19 +1,42 @@
 FlowRouter.template('/articleList', 'articleList');
 
 Template.articleList.helpers({
+    articles10: function (num) { //기사 10개씩 받아오는 함수
+        //이건이건 안돼 포기 이건 그 DB랑 연결해서 받아야겠음
+        return DB_ARTICLES.findAll({index: num}, {sort: {viewCount: -1}, limit: 10} );
+        //기사들을 조회수의 내림차순으로 return
+    },
+
+    //return _.map(_.range(0, 10), function(idx) {
+   //     return DB_ARTICLES.findAll({}, {sort: {viewCount: -1}} );
+   // });
+
+
     articles: function () {
         return DB_ARTICLES.findAll({}, {sort: {viewCount: -1}});
         //기사들을 조회수의 내림차순으로 return
     },
-    image_link: function() {
-        // 저장 된 이미지 링크를 반환
-        return DB_FILES.findOne({_id: this.image}).link();
+
+    articles00: function (n) {
+        this.articles.forEach(function(i){
+            if(i/10 === n)
+                return DB_ARTICLES.findAll({}, {sort: {viewCount: -1}});
+        });
+        //기사들을 조회수의 내림차순으로 return
     },
 
-    get_i_article: function() { //기사 하나끌고올라고 꼼지락거린 함수
+    article: function() { //기사 하나끌고올라고 꼼지락거린 함수
         return DB_ARTICLES.findOne({_id: this.createdAt}).link();
+    },
+
+    math: function(lvalue, operator, rvalue) { //index 1부터 시작하게 도와주는 함수
+        lvalue = parseInt(lvalue);
+        rvalue = parseInt(rvalue);
+
+        return { "+": lvalue + rvalue, "/": lvalue / rvalue }[operator];
     }
-    });
+
+});
 
 Template.articleList.events({
 
