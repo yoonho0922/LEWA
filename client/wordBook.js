a2afb2467d8af06cbebbd612b6bd21b478a66c17
hhz2000 <hhz2000@naver.com>
2020-05-29 12:51:12 +0900

    articleword수정

--- client/wordBook.js
+++ client/wordBook.js
@@ -13,12 +13,12 @@ Template.wordBook.helpers({
         });
         return words_list;
     },
-    titles:function () {
-        var _id=FlowRouter.getParam('_id');
-        var article_title=DB_ALL_ARTICLES.findOne({_id:article_id});
-        return article_title;
-
-    }
+    // titles:function () {
+    //     var _id=FlowRouter.getParam('_id');
+    //     var article_title=DB_ALL_ARTICLES.findOne({_id:article_id});
+    //     return article_title;
+    //
+    // }
 })
 
 
