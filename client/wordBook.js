49a1f6eedf16ada72c0be24c56000d2ce7f4e88a
hhz2000 <hhz2000@naver.com>
2020-05-29 16:25:08 +0900

    wordbook 수정

--- client/wordBook.js
+++ client/wordBook.js
@@ -1,21 +1,39 @@
 FlowRouter.template('/wordBook', 'wordBook');
 
 Template.wordBook.helpers({
-    words: function () {
+    words1: function () {
 
-        var words_list = new Array();
+        var words_list1 = new Array();
 
         // words_list.push(DB_WORDS.findOne({user_id: Meteor.user()._id}));
 
-        var listed = DB_WORDS.findAll({user_id: Meteor.user()._id});
-        listed.forEach(function (element) {
-            words_list.push(DB_WORDS.findOne({word: element.word}));
+        var listed1 = DB_WORDS.findAll({user_id: Meteor.user()._id,form:1});
+        listed1.forEach(function (element) {
+            words_list1.push(DB_WORDS.findOne({word: element.word}));
         });
-        return words_list;
+        return words_list1;
+    },
+    words2: function () {
+
+        var words_list2 = new Array();
+
+        // words_list.push(DB_WORDS.findOne({user_id: Meteor.user()._id}));
+
+        var listed2 = DB_WORDS.findAll({user_id: Meteor.user()._id,form:2});
+        listed2.forEach(function (element) {
+            words_list2.push(DB_WORDS.findOne({word: element.word}));
+        });
+        return words_list2;
     },
     titles:function (title) {
         return DB_ALL_ARTICLES.findOne({_id:title}).title;
-    }
+    },
+    math: function(lvalue, operator, rvalue) { //index 1부터 시작하게 도와주는 함수
+        lvalue = parseInt(lvalue);
+        rvalue = parseInt(rvalue);
+
+        return { "+": lvalue + rvalue, "/": lvalue / rvalue }[operator];
+    },
 })
 
 
