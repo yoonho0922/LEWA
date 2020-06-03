6b34116d11a8e65fd1ce9c903a5a217f0aad7e1d
Jungahn Yang <duckzza0921@gmail.com>
2020-06-04 04:49:07 +0900

    modify articlWord.js (홍혜진 사랑해)

--- client/details/articleWord.js
+++ client/details/articleWord.js
@@ -146,13 +146,13 @@ Template.articleWord.events({
         // findOne selector : 단어, 유저, 기사
         var conect_word=DB_WORDS.findOne({word:searchWord,user_id:user_id,form:1});
         // 현재 단어가 DB에 저장되있는지 확인
-        // word에는 null 또는 해당 단어의 object가 들어간다 (key, value의 묶음 배열)
+        // conect_word에는 null 또는 해당 단어의 object가 들어간다 (key, value의 묶음 배열)
         function getToday(){
             var date = new Date();
             return (date.getMonth()+1).toString()+"."+date.getDate().toString();
         }
 
-        function dec() {//같은 기사에서 중요한 단어라고 누른적이 있나 확인하는 함수
+        function dec() { //같은 기사 단어 중복 확인 함수
             for (i = 0; i < 100; i++) {
                 if (conect_word.article_id[i] === article_id) {
                     return '다시 누르면 삭제';
@@ -160,52 +160,38 @@ Template.articleWord.events({
             }
         }
 
-        if(!conect_word){   //null인 경우 - 단어가 저장되지 않았을경우
+        if (!conect_word) {   //null인 경우 - 단어가 저장되지 않았을경우: DB에 중복 단어 없음
             DB_WORDS.insert({
                 word: searchWord,
-                date: getToday().toString(),
-                createdAt : new Date(),
-                user_id : user_id,
-                article_id: [article_id],
-                form:1,
-                findCount:1,
+                date: getToday().toString(), //date: [getToday().toString()],
+                createdAt: new Date(),
+                user_id: user_id,
+                article_id: [article_id], //이건 뭐야..? 배열 요소로 넣는거 이렇게 표기해?
+                form: 1,
+                findCount: 1
             });
             alert("중요한 단어장에 저장");
-        }else{  //DB에 이미 있는 경우 - 삭제
-
-            if(dec()==='다시 누르면 삭제') {   //같은 기사에서 중요한 단어라고 클릭한 단어를 다시 눌러서 삭제할때
-                var remove_id=FlowRouter.getParam('_id');//현재 기사 _id 가져오기
-                var arr_length = conect_word.article_id.length;//들어가있는 article_id 배열 길이 가져오기
-
-                var res = new Array();//새로 넣어줄 배열 만들어 주기
-                for(i=0,j=0;i<arr_length;i++)
-                {
-                    if(conect_word.article_id[i]!==remove_id){
-                        res[j]=conect_word.article_id[i];
-                        j++;
-                    }
-
+        } else {  //DB에 이미 있는 경우 - 삭제 :(1. 같은 기사에서의 중복 & 2. 다른 기사에서의 중복)
+            if (dec() === '다시 누르면 삭제') { //1. 같은 기사
+                if (conect_word.findCount === 1) { //이전 기사가 한 개
+                    alert(conect_word.findCount === 1);
+                    DB_WORDS.remove({_id: conect_word._id});
+                } else { //이전 기사가 여러개
+                    DB_WORDS.update({_id: conect_word._id}, {$pull: {article_id: article_id}});
+                    DB_WORDS.update({_id: conect_word._id}, {$inc: {findCount: -1}});
+                    //DB_WORDS.update({_id: conect_word._id}, {$pop: {date: 1}});
+                    //DB_WORDS.update({_id: conect_word._id}, {$set:{date:getToday().toString()}});...?날짜 처리
                 }
-                DB_WORDS.update({_id: conect_word._id},{$set:{article_id:res}});
-                DB_WORDS.update({_id:conect_word._id},{$inc:{findCount: -1}});
-
-                if(conect_word.findCount===1){//예외처리
-                    var wordbook_remove_id=DB_WORDS.findOne({_id:conect_word._id,findCount:0})._id;
-                    DB_WORDS.remove({_id:wordbook_remove_id});
-                }
-                alert('취소되었습니다.')
-            }
-            else{
-
-                DB_WORDS.update({_id: conect_word._id},{$push:{article_id:article_id}})
-                //기존에 검색했던 (서로 다른 기사) 단어 디비 update
-                DB_WORDS.update({_id:conect_word._id},{$inc:{findCount: 1}});
-                // findCount 1씩 증가
-                DB_WORDS.update({_id:conect_word._id},{$set:{date:getToday().toString()}});
-                // 최신검색날짜 update
-                alert('중요한 단어로 등록되었습니다.')
-
+            } else { //2. 다른 기사
+                alert("중요한 단어장에 저장");
+                DB_WORDS.update({_id: conect_word._id}, {$push: {article_id: article_id}});
+                alert('업데이트')
+                DB_WORDS.update({_id: conect_word._id}, {$inc: {findCount: 1}});
+                alert('findcount증가')
+                DB_WORDS.update({_id: conect_word._id}, {$push: {date: getToday().toString()}});
+                alert('최신 등록날짜로 변경')
             }//remove는 selector가 무조건 _id여야 함
+            // alert("중요한 단어장에서 삭제");
         }
     },
     //어려운 단어 등록 버튼에 대한 함수
@@ -221,15 +207,16 @@ Template.articleWord.events({
         }
 
         // findOne selector : 단어, 유저, 기사
-        var conect_word2=DB_WORDS.findOne({word:searchWord,user_id:user_id,form:2});
+        var conect_word2 = DB_WORDS.findOne({word:searchWord,user_id:user_id,form:2});
         // 현재 단어가 DB에 저장되있는지 확인
         // word에는 null 또는 해당 단어의 object가 들어간다 (key, value의 묶음 배열)
+
         function getToday(){
             var date = new Date();
             return (date.getMonth()+1).toString()+"."+date.getDate().toString();
         }
 
-        function dec2() {//같은 기사에서 중요한 단어라고 누른적이 있나 확인하는 함수
+        function dec2() { //같은 기사 단어 중복 확인 함수
             for (i = 0; i < 100; i++) {
                 if (conect_word2.article_id[i] === article_id) {
                     return '다시 누르면 삭제';
@@ -237,52 +224,38 @@ Template.articleWord.events({
             }
         }
 
-        if(!conect_word2){   //null인 경우 - 단어가 저장되지 않았을경우
+        if (!conect_word2) {   //null인 경우 - 단어가 저장되지 않았을경우: DB에 중복 단어 없음
             DB_WORDS.insert({
                 word: searchWord,
-                date: getToday().toString(),
-                createdAt : new Date(),
-                user_id : user_id,
-                article_id: [article_id],
-                form:2,
-                findCount:1,
+                date: getToday().toString(), //date: [getToday().toString()],
+                createdAt: new Date(),
+                user_id: user_id,
+                article_id: [article_id], //이건 뭐야..? 배열 요소로 넣는거 이렇게 표기해?
+                form: 2,
+                findCount: 1
             });
-            alert("어려운 단어장에 저장");
-        }else{  //DB에 이미 있는 경우 - 삭제
-
-            if(dec2()==='다시 누르면 삭제') {   //같은 기사에서 중요한 단어라고 클릭한 단어를 다시 눌러서 삭제할때
-                var remove_id=FlowRouter.getParam('_id');//현재 기사 _id 가져오기
-                var arr_length2 = conect_word2.article_id.length;//들어가있는 article_id 배열 길이 가져오기
-
-                var res = new Array();//새로 넣어줄 배열 만들어 주기
-                for(i=0,j=0;i<arr_length2;i++)
-                {
-                    if(conect_word2.article_id[i]!==remove_id){
-                        res[j]=conect_word2.article_id[i];
-                        j++;
-                    }
-
+            alert("중요한 단어장에 저장");
+        } else {  //DB에 이미 있는 경우 - 삭제 :(1. 같은 기사에서의 중복 & 2. 다른 기사에서의 중복)
+            if (dec2() === '다시 누르면 삭제') { //1. 같은 기사
+                if (conect_word2.findCount === 1) { //이전 기사가 한 개
+                    alert(conect_word2.findCount === 1);
+                    DB_WORDS.remove({_id: conect_word2._id});
+                } else { //이전 기사가 여러개
+                    DB_WORDS.update({_id: conect_word2._id}, {$pull: {article_id: article_id}});
+                    DB_WORDS.update({_id: conect_word2._id}, {$inc: {findCount: -1}});
+                    //DB_WORDS.update({_id: conect_word._id}, {$pop: {date: 1}});
+                    //DB_WORDS.update({_id: conect_word._id}, {$set:{date:getToday().toString()}});...?날짜 처리
                 }
-                DB_WORDS.update({_id: conect_word2._id},{$set:{article_id:res}});
-                DB_WORDS.update({_id:conect_word2._id},{$inc:{findCount: -1}});
-
-                if(conect_word2.findCount===1){//예외처리
-                    var wordbook_remove_id2=DB_WORDS.findOne({_id:conect_word2._id,findCount:0})._id;
-                    DB_WORDS.remove({_id:wordbook_remove_id2});
-                }
-                alert('취소되었습니다.')
-            }
-            else{
-
-                DB_WORDS.update({_id: conect_word2._id},{$push:{article_id:article_id}});
-                //기존에 검색했던 (서로 다른 기사) 단어 디비 update
-                DB_WORDS.update({_id:conect_word2._id},{$inc:{findCount: 1}});
-                // findCount 1씩 증가
-                DB_WORDS.update({_id:conect_word2._id},{$set:{date:getToday().toString()}});
-                // 최신검색날짜 update
-                alert('어려운 단어로 등록되었습니다.')
-
+            } else { //2. 다른 기사
+                alert("중요한 단어장에 저장");
+                DB_WORDS.update({_id: conect_word2._id}, {$push: {article_id: article_id}});
+                alert('업데이트')
+                DB_WORDS.update({_id: conect_word2._id}, {$inc: {findCount: 1}});
+                alert('findcount증가')
+                DB_WORDS.update({_id: conect_word2._id}, {$push: {date: getToday().toString()}});
+                alert('최신 등록날짜로 변경')
             }//remove는 selector가 무조건 _id여야 함
+            // alert("중요한 단어장에서 삭제");
         }
     },
 
@@ -293,6 +266,21 @@ Template.articleWord.events({
         // Session.set('tag_arr', tag_update);
         DB_WORDS.remove({_id: this._id});
         alert('삭제 되었습니다.');
+
+
+        /* 이걸로 단어 태그 지우면 단어 한개씩 없어짐: 근데 어떤 tag를 선택했는지 설정을 못하겠어ㅜㅜ(바꿀 때 conect_word 나 conect_word2 )
+        if (conect_word2.findCount === 1) { //이전 기사가 한 개
+            alert(conect_word2.findCount === 1);
+            DB_WORDS.remove({_id: conect_word2._id});
+        } else { //이전 기사가 여러개
+            DB_WORDS.update({_id: conect_word2._id}, {$pull: {article_id: article_id}});
+            DB_WORDS.update({_id: conect_word2._id}, {$inc: {findCount: -1}});
+            //DB_WORDS.update({_id: conect_word._id}, {$pop: {date: 1}});
+            //DB_WORDS.update({_id: conect_word._id}, {$set:{date:getToday().toString()}});...?날짜 처리
+        }
+
+         */
+
     }
 
 });
